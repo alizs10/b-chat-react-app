bb3a5370b5cdabba10a429f91ed018fbb86610cb
alizs10 <ali.text77@gmail.com>
2022-08-25 13:23:57 +0430

    fix sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,13 +3,14 @@ import Conversations from './Sidebar/Conversations'
 
 function Sidebar() {
 
-    const handleNewConversationHover = () => {
-        console.log(newConversationBtnRef.current.classList);
-        newConversationBtnRef.current.classList.remove('w-fit')
-        newConversationBtnRef.current.classList.add('w-full')
+    const handleMouseOver = () => {
+        newConversationTextRef.current.classList.remove('hidden');
+    }
+    const handleMouseLeave = () => {
+        newConversationTextRef.current.classList.add('hidden');
     }
 
-    const newConversationBtnRef = useRef(null)
+    const newConversationTextRef = useRef(null)
 
     return (
         <div className="relative flex flex-col gap-y-2 h-screen bg-white col-span-3">
@@ -20,11 +21,11 @@ function Sidebar() {
 
             <Conversations />
 
-            <button  ref={newConversationBtnRef} className="absolute bottom-5 left-5 z-30 flex items-center w-[4.5rem] hover:w-4/5 p-[7px] rounded-corners bg-[#5F7AF0] btn-hover text-white transition-all duration-300">
-                <span className='p-3 rounded-corners bg-[#4361EE] px-4 flex-center'>
+            <button onMouseOver={() => handleMouseOver()} onMouseLeave={() => handleMouseLeave()} className="absolute bottom-5 left-5 z-30 flex items-center w-[4.5rem] hover:w-4/5 p-[7px] rounded-corners bg-[#5F7AF0] btn-hover text-white transition-all duration-300">
+                <span className='p-3 w-fit rounded-corners bg-[#4361EE] px-4 flex-center'>
                     <i className="fa-regular fa-comment-medical text-2xl"></i>
                 </span>
-                <span className='ml-2 whitespace-nowrap text-lg'>Start New Conversation</span>
+                <span ref={newConversationTextRef} className='hidden ml-2 whitespace-nowrap text-lg'>Start New Conversation</span>
             </button>
 
         </div>
