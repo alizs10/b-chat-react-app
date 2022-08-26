3b1c20224290f33edcdf0e0bd6118318ebe18439
alizs10 <ali.text77@gmail.com>
2022-08-26 19:33:25 +0430

    update: alerts popup

--- src/components/Chat/Head/AlertsPopup.js
+++ src/components/Chat/Head/AlertsPopup.js
@@ -2,19 +2,22 @@ import React from 'react'
 
 function AlertsPopup() {
   return (
-    <div className="w-48 overflow-hidden flex flex-col absolute right-0 top-20 z-30 shadow-md bg-white rounded-corners">
+    <div className="w-56 overflow-hidden flex flex-col absolute right-0 top-20 z-30 shadow-md bg-white rounded-corners">
             <button className="text-right hover:bg-gray-100 transition-all duration-300 flex-center gap-x-2 py-3 text-xs text-gray-800">
-                <i className="fa-regular fa-circle-user"></i>
-                <span>Your Profile</span>
+                <span className="w-2 h-2 rounded-full bg-emerald-500"></span>
+                <span>new message from @alizs10</span>
             </button>
-            <button className="hover:bg-gray-100 transition-all duration-300 flex-center gap-x-2 py-3 text-xs text-gray-800">
-                <i className="fa-regular fa-gear"></i>
-                <span>Settings</span>
+           
+            <button className="text-right hover:bg-gray-100 transition-all duration-300 flex-center gap-x-2 py-3 text-xs text-gray-800">
+                <span className="w-2 h-2 rounded-full bg-emerald-500"></span>
+                <span>new message from @alizs10</span>
             </button>
-            <button className="hover:bg-gray-100 transition-all duration-300 flex-center gap-x-2 py-3 text-xs text-gray-800">
-                <i className="fa-regular fa-arrow-right-from-bracket"></i>
-                <span>Logout</span>
+           
+            <button className="text-right hover:bg-gray-100 transition-all duration-300 flex-center gap-x-2 py-3 text-xs text-gray-800">
+                <span className="w-2 h-2 rounded-full bg-emerald-500"></span>
+                <span>new message from @alizs10</span>
             </button>
+           
         </div>
   )
 }
