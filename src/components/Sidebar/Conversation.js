43623ac64a7895068c85e25c4c75d22131ef432c
alizs10 <ali.text77@gmail.com>
2022-08-27 20:12:48 +0430

    update: conversations

--- src/components/Sidebar/Conversation.js
+++ src/components/Sidebar/Conversation.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
-function Conversation({ username, last_message, date, avatar, status, active = false }) {
+function Conversation({ conversation, active, setActiveConversation}) {
     return (
-        <li className={`cursor-pointer transition-all duration-300 ${active ? 'bg-[#abc4ff] ' : 'hover:bg-gray-100 text-gray-700'} border-b border-gray-100 flex gap-2 w-full p-3 rounded-corners `}>
+        <li onClick={() => setActiveConversation(conversation.id)} className={`cursor-pointer transition-all duration-300 ${active ? 'bg-[#abc4ff] ' : 'hover:bg-gray-100 text-gray-700'} border-b border-gray-100 flex gap-2 w-full p-3 rounded-corners `}>
             <div className='relative cursor-pointer w-16'>
-                <img className='rounded-corners w-full' src={avatar} />
+                <img className='rounded-corners w-full' src={conversation.avatar} />
                 <span className='absolute -bottom-1 -right-0 bg-white p-[3px] flex-center rounded-full'>
-                    <span className={`w-3 h-3 rounded-full ${status ? "bg-emerald-500" : "bg-gray-400"}`}></span>
+                    <span className={`w-3 h-3 rounded-full ${conversation.status ? "bg-emerald-500" : "bg-gray-400"}`}></span>
                 </span>
             </div>
             <div className='w-[85%] flex flex-col gap-y-2'>
                 <div className='flex justify-between items-center'>
-                    <span className='font-bold text-lg'>{username}</span>
-                    <span className='text-sm text-gray-600'>{date}</span>
+                    <span className='font-bold text-lg'>{conversation.username}</span>
+                    <span className='text-sm text-gray-600'>{conversation.date}</span>
                 </div>
-                <span className='text-sm'>{last_message}</span>
+                <span className='text-sm'>{conversation.last_message}</span>
             </div>
         </li>
     )
--- src/components/Sidebar/Conversations.js
+++ src/components/Sidebar/Conversations.js
@@ -1,24 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Conversation from './Conversation'
 
 function Conversations() {
+
+    const [conversations, setConversations] = useState([
+        {
+            id: 1,
+            status: 1, // 0 => offline, 1 => online
+            last_message: "How You Doing?? MISS YOU!",
+            username: "Matt Leblanc",
+            date: "2:45 PM",
+            avatar: "assets/images/user-profile-1.webp"
+        },
+        {
+            id: 2,
+            status: 0, // 0 => offline, 1 => online
+            last_message: "Come back GOAT :)",
+            username: "Cristiano",
+            date: "yesterday",
+            avatar: "assets/images/user-profile-2.webp"
+        },
+        {
+            id: 3,
+            status: 0, // 0 => offline, 1 => online
+            last_message: "So Handsome",
+            username: "David Beckham",
+            date: "2022/08/16",
+            avatar: "assets/images/user-profile-3.webp"
+        },
+        {
+            id: 4,
+            status: 0, // 0 => offline, 1 => online
+            last_message: "Big win! have fun...",
+            username: "Johnny Depp",
+            date: "2022/08/12",
+            avatar: "assets/images/user-profile-4.webp"
+        },
+        {
+            id: 5,
+            status: 0, // 0 => offline, 1 => online
+            last_message: "LEGEND!",
+            username: "Mehran Modiri",
+            date: "2022/08/01",
+            avatar: "assets/images/user-profile-5.webp"
+        },
+    ])
+    const [activeConversation, setActiveConversation] = useState(2)
+
     return (
 
         <div className='relative flex flex-col gap-y-2 pl-3 styled-scrollbar overflow-y-scroll'>
 
-            <span className="bg-white/80 z-20 sticky top-0 block text-sm text-gray-600 pb-1 border-b border-gray-200">Conversations (8)</span>
+            <span className="bg-white/80 z-20 sticky top-0 block text-sm text-gray-600 pb-1 border-b border-gray-200">Conversations ({conversations.length})</span>
             <ul className='mt-2 flex flex-col gap-y-2 pb-3 mr-3'>
-                <Conversation status={false} last_message="LEGEND!" username="Mehran Modiri" date="2022/08/16" avatar="assets/images/user-profile-5.webp" />
-                <Conversation status={true} active={true} last_message="How u doing?? Miss u man..." username="Matt LeBlanc" date="2:04 PM" avatar="assets/images/user-profile-1.webp" />
-                <Conversation status={false} last_message="Hello Goat!!! Siuuuuuu" username="Cristiano" date="yesterday" avatar="assets/images/user-profile-2.webp" />
-                <Conversation status={false} last_message="So HANDSOME..." username="David Beckham" date="yesterday" avatar="assets/images/user-profile-3.webp" />
-                <Conversation status={false} last_message="Congratulations man!!! Big Victory" username="Johnny Depp" date="2022/08/25" avatar="assets/images/user-profile-4.webp" />
-                <Conversation status={false} last_message="Congratulations man!!! Big Victory" username="Johnny Depp" date="2022/08/25" avatar="assets/images/user-profile-4.webp" />
-                <Conversation status={false} last_message="Congratulations man!!! Big Victory" username="Johnny Depp" date="2022/08/25" avatar="assets/images/user-profile-4.webp" />
-                <Conversation status={false} last_message="Congratulations man!!! Big Victory" username="Johnny Depp" date="2022/08/25" avatar="assets/images/user-profile-4.webp" />
-                <Conversation status={false} last_message="Congratulations man!!! Big Victory" username="Johnny Depp" date="2022/08/25" avatar="assets/images/user-profile-4.webp" />
-                <Conversation status={false} last_message="Congratulations man!!! Big Victory" username="Johnny Depp" date="2022/08/25" avatar="assets/images/user-profile-4.webp" />
-
+                {conversations.map(conversation => (
+                    <Conversation setActiveConversation={setActiveConversation} conversation={conversation} active={activeConversation == conversation.id ? true : false}/>
+                ))}
             </ul>
         </div>
 
