8c533b57cc10f02d9bf66f100744ef2579431c08
alizs10 <ali.text77@gmail.com>
2022-08-27 20:57:51 +0430

    update: sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,30 +3,27 @@ import Conversations from './Sidebar/Conversations'
 
 function Sidebar() {
 
-    const handleMouseOver = () => {
-        newConversationTextRef.current.classList.remove('hidden');
-    }
-    const handleMouseLeave = () => {
-        newConversationTextRef.current.classList.add('hidden');
-    }
-
-    const newConversationTextRef = useRef(null)
-
     return (
-        <div className="relative flex flex-col gap-y-2 h-screen bg-white col-span-3">
+        <div className="relative grid grid-rows-6 gap-y-2 h-screen bg-white col-span-3">
 
-            <div className=" flex flex-col gap-y-2 p-3">
-                <span className='font-bold text-2xl py-2 block'><span className="text-[#4361ee]">B</span>CHAT</span>
+            <div className='row-span-5 flex flex-col gap-y-2'>
+
+                <div className="flex flex-col gap-y-2 p-3">
+                    <span className='font-bold text-2xl py-2 block'><span className="text-[#4361ee]">B</span>CHAT</span>
+                </div>
+
+                <Conversations />
             </div>
 
-            <Conversations />
+            <div className='row-span-1 m-4'>
 
-            <button onMouseOver={() => handleMouseOver()} onMouseLeave={() => handleMouseLeave()} className="absolute bottom-5 left-5 z-30 flex items-center w-[4.5rem] hover:w-4/5 p-[7px] rounded-corners bg-[#5F7AF0] btn-hover text-white transition-all duration-300">
-                <span className='p-3 w-fit rounded-corners bg-[#4361EE] px-4 flex-center'>
-                    <i className="fa-regular fa-comment-medical text-2xl"></i>
-                </span>
-                <span ref={newConversationTextRef} className='hidden ml-2 whitespace-nowrap text-lg'>Start New Conversation</span>
-            </button>
+                <button className="flex w-full items-center p-2 rounded-corners bg-[#5F7AF0] btn-hover text-white transition-all duration-300">
+                    <span className='p-3 w-fit rounded-corners bg-[#4361EE] px-4 flex-center'>
+                        <i className="fa-regular fa-comment-medical text-2xl"></i>
+                    </span>
+                    <span className='ml-2 whitespace-nowrap text-lg'>Start New Conversation</span>
+                </button>
+            </div>
 
         </div>
     )
