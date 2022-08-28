eb40ddd24580628e1c66588457df2d80b9c0e795
alizs10 <ali.text77@gmail.com>
2022-08-28 13:54:49 +0430

    update: responive head

--- src/components/Chat/SearchInChat.js
+++ src/components/Chat/SearchInChat.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SearchInChat() {
     return (
-        <div className='col-span-6 flex items-center px-4'>
-            <div className='flex bg-[#E4E9FD] text-[#1C42EA] h-3/5 rounded-corners ml-10 w-1/2'>
+        <div className='col-span-5 lg:col-span-6 flex items-center px-4'>
+            <div className='flex bg-[#E4E9FD] text-[#1C42EA] h-3/5 rounded-corners lg:ml-10 w-full lg:w-1/2'>
                 <span className='w-[20%] flex-center text-xl'>
                     <i className="fa-regular fa-magnifying-glass"></i>
                 </span>
