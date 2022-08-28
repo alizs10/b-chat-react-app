15b3c3d6b20256d4cc79f0421c10c8371cde9ac8
alizs10 <ali.text77@gmail.com>
2022-08-28 19:20:25 +0430

    update: auth responsive

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -10,7 +10,7 @@ function Auth() {
   return (
     <div className="w-screen h-screen bg-white flex-center flex-col gap-y-2">
 
-      <span className='text-3xl font-bold'><span className="text-[#1C42EA]">BUSINESS </span>CHAT <i className="fa-regular fa-comment-dollar"></i></span>
+      <span className='text-2xl lg:text-3xl font-bold'><span className="text-[#1C42EA]">BUSINESS </span>CHAT <i className="fa-regular fa-comment-dollar"></i></span>
       {(!loginFormVisibility && !signupFormVisibility) && (
         <button onClick={() => setLoginFormVisibility(true)} className='mt-10 flex gap-x-2 items-center py-3 px-5 rounded-corners bg-[#4361EE] btn-hover text-white transition-all duration-300'>
           <span className='text-lg'>LET'S START</span>
