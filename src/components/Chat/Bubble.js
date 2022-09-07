d41758f548318fdea35079926081c1f682db4097
alizs10 <ali.text77@gmail.com>
2022-09-07 15:55:12 +0430

    update: chat bubbles

--- src/components/Chat/Bubble.js
+++ src/components/Chat/Bubble.js
@@ -25,18 +25,21 @@ function Bubble() {
     return (
         <div className="ml-2 flex gap-x-4 items-end">
             <div className='relative flex flex-col gap-y-1 w-fit max-w-[70%]'>
-                <span className="text-gray-600 ml-2">Matt LeBlanc</span>
+                <span className='cursor-pointer w-fit flex gap-x-1 items-center'>
+                    <img className='w-8 rounded-corners' src='./assets/images/user-profile-1.webp' />
+                    <span className="text-gray-600 ml-2">Matt LeBlanc</span>
+                </span>
 
-                <div onMouseOver={() => handleMouseOver()} onMouseLeave={() => handleMouseLeave()} className="relative bg-white p-3 border-2 border-gray-200 rounded-corners w-full text-gray-800 text-base">
+                <div onMouseOver={() => handleMouseOver()} onMouseLeave={() => handleMouseLeave()} className="relative ml-8 bg-white p-3 border-2 border-gray-200 rounded-corners w-full text-gray-800 text-base">
                     <p>
                         lorem ipsum dolor
                     </p>
-                {replayBtnVisibility && (<Replay setReplayBtnVisibility={setReplayBtnVisibility} replayRemover={replayRemover} replayBtnRef={replayBtnRef} />)}
+                    {replayBtnVisibility && (<Replay setReplayBtnVisibility={setReplayBtnVisibility} replayRemover={replayRemover} replayBtnRef={replayBtnRef} />)}
                 </div>
 
 
             </div>
-            <div className='flex items-end gap-x-2'>
+            <div className='flex items-end gap-x-2 ml-8'>
                 <span className="text-[12px] mb-1">
                     <span className="text-gray-600">2:43 PM</span>
                 </span>
--- src/components/Chat/BubbleWithReplay.js
+++ src/components/Chat/BubbleWithReplay.js
@@ -24,9 +24,11 @@ function BubbleWithReplay() {
     return (
         <div className="ml-2 flex gap-x-4 items-end">
             <div className='relative flex flex-col gap-y-1 w-fit max-w-[70%]'>
-                <span className="text-gray-600 ml-2">Matt LeBlanc</span>
-
-                <div onMouseOver={() => handleMouseOver()} onMouseLeave={() => handleMouseLeave()} className="flex flex-col bg-white border-b-2 border-gray-200 rounded-corners w-full text-gray-800 text-base">
+                <span className='cursor-pointer w-fit flex gap-x-1 items-center'>
+                    <img className='w-8 rounded-corners' src='./assets/images/user-profile-1.webp' />
+                    <span className="text-gray-600 ml-2">Matt LeBlanc</span>
+                </span>
+                <div onMouseOver={() => handleMouseOver()} onMouseLeave={() => handleMouseLeave()} className="ml-8 flex flex-col bg-white border-b-2 border-gray-200 rounded-corners w-full text-gray-800 text-base">
                     <div className="relative flex flex-col gap-y-2 border-l-4 rounded-t-[25px] border-l-[#4361ee] pl-3 pt-3 px-3 pb-2 border-b text-xs">
                         <span>You</span>
                         <span>lorem ipsum dolor sit amet, consectetur adip</span>
@@ -37,7 +39,7 @@ function BubbleWithReplay() {
 
 
             </div>
-            <div className='flex items-end gap-x-2'>
+            <div className='flex items-end gap-x-2 ml-8'>
                 <span className="text-[12px] mb-1">
                     <span className="text-gray-600">2:43 PM</span>
                 </span>
