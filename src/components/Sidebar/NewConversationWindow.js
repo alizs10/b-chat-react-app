428faa927aa6a141186ddef02da9b7f83b07ef73
alizs10 <ali.text77@gmail.com>
2022-09-08 17:46:20 +0430

    fix

--- src/components/Sidebar/NewConversationWindow.js
+++ src/components/Sidebar/NewConversationWindow.js
@@ -4,7 +4,7 @@ function NewConversationWindow({ handleClose }) {
   return (
     <div
       onClick={e => e.stopPropagation()}
-      className='z-50  w-4/5 md:w-2/5 lg:w-1/5 pb-5 p-3 rounded-corners flex-col gap-y-4 bg-white shadow-lg'>
+      className='z-50  w-4/5 md:w-2/5 lg:w-1/5 px-5 py-3 rounded-corners flex-col gap-y-4 bg-white shadow-lg'>
       <div className='flex justify-between items-center border-b border-gray-200 pb-1'>
         <span className="text-sm text-gray-800">Start A New Conversation</span>
         <span onClick={() => handleClose(false)} className='cursor-pointer flex-center w-8 h-8 text-xs rounded-full hover:bg-gray-200 transition-all duration-300'>
@@ -23,7 +23,7 @@ function NewConversationWindow({ handleClose }) {
           />
         </div>
 
-        <button className='btn-hover w-full py-3 bg-[#4361EE] text-white text-sm rounded-corners'>
+        <button className='mt-2 btn-hover w-full py-3 bg-[#4361EE] text-white text-sm rounded-corners'>
           Start Conversation
         </button>
 
