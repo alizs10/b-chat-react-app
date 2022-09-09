61e8e57b1180bf3640a4cc785f98c64aeaa876c3
alizs10 <ali.text77@gmail.com>
2022-09-10 00:07:20 +0430

    fix

--- src/components/Sidebar/Conversations.js
+++ src/components/Sidebar/Conversations.js
@@ -1,3 +1,4 @@
+import { isEmpty } from 'lodash'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Conversation from './Conversation'
@@ -55,12 +56,14 @@ function Conversations({ setSidebarVisibility }) {
 
         <div className='h-full relative flex flex-col gap-y-2 pl-3 styled-scrollbar overflow-y-scroll'>
 
-            <span className="bg-white/80 z-20 sticky top-0 block text-sm text-gray-600 pb-1 border-b border-gray-200">Conversations ({conversations.length})</span>
-            <ul className='mt-2 flex flex-col gap-y-2 pb-3 mr-3'>
-                {conversations.map(conversation => (
-                    <Conversation setSidebarVisibility={setSidebarVisibility} setActiveConversation={setActiveConversation} conversation={conversation} active={activeConversation == conversation.id ? true : false} />
-                ))}
-            </ul>
+            <span className="bg-white/80 z-20 sticky top-0 block text-sm text-gray-600 pb-1 border-b border-gray-200">Conversations ({!isEmpty(conversations) ? conversations.length : 0})</span>
+            {!isEmpty(conversations) && (
+                <ul className='mt-2 flex flex-col gap-y-2 pb-3 mr-3'>
+                    {conversations.map(conversation => (
+                        <Conversation key={conversation.id} setSidebarVisibility={setSidebarVisibility} setActiveConversation={setActiveConversation} conversation={conversation} active={activeConversation == conversation.id ? true : false} />
+                    ))}
+                </ul>
+            )}
         </div>
 
     )
